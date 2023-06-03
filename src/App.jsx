87d8c38db8498b468537f32045c297d7b9a72aa0
test <test@pganjac.com>
2023-06-03 11:33:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./card"
+const app=()=>{
+  return(
+    <>
+    <h1>I am Mad</h1>
+    <Card name='welcome'/>
+    {/* <img src="thala-ajith-1619846161.jpg" alt=""/>  */}
+    </>
+  )
+  
+}
+export default app
